ad27db0492d4fbe65821cb93f6313f8cfcdf94c8
sumit kumar <sumit1523@gmail.com>
2024-03-01 00:53:41 +0530

    build components

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import appwriteService from "../appwrite/config";
+import appwriteService from "../appwrite/auth";
 import { Link } from "react-router-dom";
 
 function PostCard({ $id, title, featuredImage }) {
