0e999cfbb631bd85b0f25bab0333c31cf3d75196
sumit kumar <sumit1523@gmail.com>
2024-02-29 15:40:05 +0530

    Header and Footer Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
+import { useState, useEffect } from "react";
 import "./App.css";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Header, Footer } from "./components";
+// import { Outlet } from "react-router-dom";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL, "hii");
-  return (
-    <>
-      <h1>Blog App</h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) dispatch(login(userData));
+        else dispatch(logout());
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-sc flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>TODO{/* <Outlet /> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
