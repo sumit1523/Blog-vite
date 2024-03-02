ae8d61c1c9e38b03686116343cad53a36476ac49
sumit kumar <sumit1523@gmail.com>
2024-03-02 16:12:06 +0530

    env samaple

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
 const store = configureStore({
-  reducer: { auth: authReducer },
+  reducer: {
+    auth: authReducer,
+    // post: postSlice
+  },
 });
 
 export default store;
