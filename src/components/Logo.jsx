d200dd9371fac74ca62eead0b23777bb9a8c0849
sumit kumar <sumit1523@gmail.com>
2024-03-02 16:34:45 +0530

    logo added

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import logoImg from "../assets/react.svg";
 const Logo = ({ width = "100px" }) => {
-  return <div>Logo</div>;
+  return (
+    <div>
+      <img src={logoImg} alt="Blo-vite"></img>
+    </div>
+  );
 };
 
 export default Logo;
